20813f9b1f3e5ea1001b355ad109bc7a0b8c58f8
Sabbir Ahmed <sabbiirr@gmail.com>
2014-12-11 00:42:10 +0600

    init

---
+++ Events.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Events
+Plugin URI: http://github.com/thesabbir/events
+Description: Light weight event posting plugin.
+Version: 1.0
+Author: Sabbir Ahmed
+Author URI: http://twitter.com/alreadysabbir
+License: GPLv2 or Later
+*/
+
