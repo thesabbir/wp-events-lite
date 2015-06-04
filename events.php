7ab766c11e891da1e8dfee7d811e30ba441d606a
Sabbir Ahmed <sabbiirr@gmail.com>
2015-06-04 16:32:58 +0600

    Update and rename Events.php to events.php
    Corrected version number to  1.0

--- events.php
+++ events.php
@@ -4,7 +4,7 @@
 Plugin Name: Events Lite
 Plugin URI: https://github.com/thesabbir/wp-events-lite
 Description: Light weight event posting plugin.
-Version: 1.1
+Version: 1.0
 Author: Sabbir Ahmed
 Author URI: http://twitter.com/alreadysabbir
 License: GPLv2 or Later
